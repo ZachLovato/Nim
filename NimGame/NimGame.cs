eb5a2c003168fafa906a0cf654019b1c049baf7c
AReeme <areeme@student.neumont.edu>
2024-01-17 18:53:36 -0700

    NimGame.cs Created

---
+++ NimGame/NimGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimGame
+{
+    internal class NimGame
+    {
+        public bool isGameOver;
+        public bool isPlayerOneTurn;
+        private int[][] pegs;
+
+        public void switchPlayerTurn()
+        {
+
+        }
+
+        public void checkGameEnd()
+        {
+
+        }
+
+        public void removePegs(int pegs1, int pegs2)
+        {
+
+        }
+
+        public int pegsRemaining(int pegs1, int pegs2)
+        {
+            return 0;
+        }
+    }
+}
